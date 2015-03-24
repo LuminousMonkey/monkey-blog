b1a7cda01f633dca6b02a901b4dc32cd17bbc0f9
Mike Aldred <mike.aldred@luminousmonkey.org>
2015-03-24 15:28:01 +0800

    Should ignore temp Emacs file.

--- config.clj
+++ config.clj
@@ -9,4 +9,15 @@
  :default-extension "html"
  :encoding "UTF-8"
  :posts-per-page 5
- :blog-as-index true]
+ :blog-as-index true
+ :emacs "/usr/bin/emacs"
+ :org-export-command "(progn
+                       (package-initialize)
+                       (require 'use-package)
+                       (require 'ox-html)
+                       (use-package clojure-mode)
+                       (setq org-html-htmlize-font-prefix \"\")
+                       (setq org-html-htmlize-output-type 'css)
+                       (org-html-export-as-html nil nil nil t nil)
+                       (with-current-buffer \"*Org HTML Export*\"
+                         (princ (org-no-properties (buffer-string)))))"]
