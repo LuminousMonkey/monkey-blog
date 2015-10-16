289aa6d9190f9849d2b5d09c6772a3aad11aff7c
Mike Aldred <mike.aldred@luminousmonkey.org>
2015-10-16 16:32:41 +0800

    Updated about page.

--- resources/site/about.clj
+++ resources/site/about.clj
@@ -2,4 +2,13 @@
 
 [:div.one-column
  [:h2 "About"]
- [:p "I don't feel comfortable describing myself, because, really, I'm around myself all day. This blog is mainly a collection of thoughts that occur to me, usually about programming or software development."]]
+ [:h3 "Who's Luminousmonkey?"]
+ [:p "I'm Mike Aldred. I was born in Sydney, Australia, but my family moved over to New Zealand when I was two years old. When I was about twelve years old we moved over to Perth, Australia, where I'm currently living. Eventually I got around to Curtin University where I studied computer science, and I have to be honest, I lost interest and didn't do that well."]
+ [:p "I worked as an IT support technician, fixing computers for companies, until I started working for a small startup, Minetec, where I wrote some minor software and was its sole employee. From there, I started working at Direct Communications, where I started as a technical hand/IT support, moved to bookkeeping for a few years, and now I'm currently the general manager."]
+ [:p "After ten years of working at Direct Communications, I decided to finish off my computer science degree, so I'm currently studying on a part-time basis."]
+
+ [:h3 "What's it about?"]
+ [:p "It's just a site for me to write down some thoughts. Mostly interesting technical things, however, I might occasionally post something business related. There may also be the occasional rant."]
+
+ [:h3 "What's on this site?"]
+ [:p "Nothing much, I'm currently trying to build the habit of posting regularly on it, so there won't be much of a cohesive theme. But I want to work on writing up some articles, mostly on my experiences in small business. In particular the poor state of accounting software for our particular company."]]
