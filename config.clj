a6444f6a6af9c9dcacdf18fc7b07a78b39f00b63
Mike Aldred <mike.aldred@luminousmonkey.org>
2016-02-23 15:00:20 +0800

    Updated to support adding images in org mode.

--- config.clj
+++ config.clj
@@ -20,6 +20,14 @@
                        (eval-and-compile
                          (require 'use-package)
                          (require 'ox-html))
+                       (defun org-custom-link-img-follow (path)
+                         (org-open-file-with-emacs
+                          (format \"../images/%s\" path)))
+                       (defun org-custom-link-img-export (path desc format)
+                         (cond
+                          ((eq format 'html)
+                           (format \"<img src=\\\"/images/%s\\\" alt=\\\"%s\\\">\" path desc))))
+                       (org-add-link-type \"img\" 'org-custom-link-img-follow 'org-custom-link-img-export)
                        (use-package clojure-mode :load-path \"site-lisp/clojure-mode\")
                        (setq org-html-htmlize-font-prefix \"\")
                        (setq org-html-htmlize-output-type 'css)
