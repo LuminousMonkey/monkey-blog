af6511d51615d85cd3b2667ec74bb0b8a57c6855
Mike Aldred <mike.aldred@luminousmonkey.org>
2018-10-16 19:08:34 +0800

    Updated about page and post on audio.

--- resources/site/about.clj
+++ resources/site/about.clj
@@ -6,7 +6,7 @@
  [:p "I'm Mike Aldred. I was born in Sydney, Australia, but my family moved over to New Zealand when I was two years old. When I was about twelve years old we moved over to Perth, Australia, where I'm currently living. Eventually I got around to Curtin University where I studied computer science, and I have to be honest, I lost interest and didn't do that well."]
  [:p "I worked as an IT support technician, fixing computers for companies, until I started working for a small startup, Minetec, where I wrote some minor software and was its sole employee. From there, I started working at Direct Communications, where I started as a technical hand/IT support, moved to bookkeeping for a few years, and now I'm currently the general manager."]
  [:p "After ten years of working at Direct Communications, I decided to finish off my computer science degree, so I'm currently studying on a part-time basis."]
- [:p "I stream playing games on Twitch " [:a {:href "https://www.twitch.tv/LuminousMonkey"}] " some Retro Commodore 64 and Amiga games, and Quake Champions, things like that."]
+ [:p "I stream playing games on Twitch " [:a {:href "https://www.twitch.tv/LuminousMonkey"} "(LuminousMonkey on Twitch)"] ", including some Retro Commodore 64 and Amiga games, and Quake Champions, things like that."]
 
  [:h3 "What's it about?"]
  [:p "It's just a site for me to write down some thoughts. Mostly interesting technical things, however, I might occasionally post something business related. There may also be the occasional rant."]
